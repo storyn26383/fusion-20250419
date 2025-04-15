44c5916c5c5b45849d8fb32892fd3949097e01a5
Sasaya <storyn26383@gmail.com>
2025-04-15 23:09:04 +0800

    feat: counter

---
+++ resources/js/Pages/Counter.vue
@@ -0,0 +1,11 @@
+<php>
+    $count = prop(0);
+    $doubleCount = prop(fn () => $count * 2)->readonly();
+</php>
+
+<template>
+    <button @click="count++; fusion.sync()">
+        <h1>Count: {{ count }}</h1>
+    </button>
+    <h1>Double Count: {{ doubleCount }}</h1>
+</template>
