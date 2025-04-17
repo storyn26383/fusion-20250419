c98dec9a697793e9650b9b9f23f570981b7e0316
Sasaya <storyn26383@gmail.com>
2025-04-17 23:32:45 +0800

    feat: action

---
+++ resources/js/Pages/Action.vue
@@ -0,0 +1,16 @@
+<php>
+    expose(getHeader: fn () => request()->header('User-Agent'));
+</php>
+
+<script setup>
+    import { ref } from 'vue'
+    const result = ref('')
+    async function getResult() { result.value = await getHeader() }
+</script>
+
+<template>
+    <button @click="getResult">
+        <h1>Get User Agent from Server</h1>
+    </button>
+    <h1 v-if="result">{{ result }}</h1>
+</template>
---
+++ storage/fusion/JavaScript/Pages/Action.js
@@ -0,0 +1,31 @@
+import Pipeline from '@fusion/vue/pipeline';
+import ActionFactory from '@fusion/vue/actionFactory';
+
+
+export const state = [];
+export const actions = ["getHeader"];
+export const fusionActions = ["fusionSync"];
+
+let cachedState;
+
+export function useFusion(keys = [], props = {}, useCachedState = false) {
+  const state = (useCachedState && cachedState) ? cachedState : new Pipeline(props).createState();
+
+  if (!useCachedState) {
+    cachedState = state;
+  }
+
+  const all = {
+    ...state,
+    ...new ActionFactory([...actions, ...fusionActions], state),
+  }
+
+  const shouldExport = {};
+  for (const key of keys) {
+    if (key in all) {
+      shouldExport[key] = all[key];
+    }
+  }
+
+  return shouldExport;
+}
